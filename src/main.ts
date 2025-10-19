8a907df8ad33a74e1e55805dc11d26c9b080cfd6
Spectrum <113600641+SpectrumForevers@users.noreply.github.com>
2025-10-19 16:56:01 +0300

    add character and animation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import Phaser from "phaser";
 import './style.css';
+import { scenes } from "./scenes";
 
 new Phaser.Game({
   width: 800,
   height: 600,
   title: 'Phaser RPG',
+  scene: scenes,
   url: import.meta.env.URL || '',
   version: import.meta.env.VERSION || '0.0.1',
   backgroundColor: '#000000ff',
